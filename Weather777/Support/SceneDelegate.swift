c6df52039133ce76745066af62f3ca558a5aed20
김두운 <dave17code@MacBookAir15.local>
2024-02-13 21:02:26 +0900

    [Feat]#10-MainVCAutoLayoutMacBookAir15

--- Weather777/Support/SceneDelegate.swift
+++ Weather777/Support/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootVC = ViewController() // 추후 해당 뷰컨으로 조정
+        let rootVC = MainViewController() // 추후 해당 뷰컨으로 조정
         window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
     }
