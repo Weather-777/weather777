ac27aae188d8e4047ce0aed91b1d31bc9cfbcbbf
Darwin <limsj318@naver.com>
2024-02-13 13:25:42 +0900

    프리뷰 위치 조정

--- Weather777/WeatherList/WeatherListViewController.swift
+++ Weather777/WeatherList/WeatherListViewController.swift
@@ -6,8 +6,39 @@
 //
 
 import UIKit
-import SwiftUI
 import MapKit
+import SwiftUI
+
+// MARK: - PreView
+struct PreView: PreviewProvider
+{
+    static var previews: some View
+    {
+        WeatherListViewController().toPreview()
+    }
+}
+
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable
+    {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController
+        {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
+    }
+
+    func toPreview() -> some View
+    {
+        Preview(viewController: self)
+    }
+}
+#endif
 
 class WeatherListViewController: UIViewController
 {
@@ -335,34 +366,3 @@ extension WeatherListViewController: UITableViewDataSource, UITableViewDelegate
     
     
 }
-
-
-struct PreView: PreviewProvider
-{
-    static var previews: some View 
-    {
-        WeatherListViewController().toPreview()
-    }
-}
-
-
-#if DEBUG
-extension UIViewController {
-    private struct Preview: UIViewControllerRepresentable 
-    {
-        let viewController: UIViewController
-
-        func makeUIViewController(context: Context) -> UIViewController
-        {
-            return viewController
-        }
-
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
-    }
-
-    func toPreview() -> some View
-    {
-        Preview(viewController: self)
-    }
-}
-#endif
