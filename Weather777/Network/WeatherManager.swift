1606354dccc4093495afcde3e99c2a4f894504cd
JasonYang <didwnsgus@gmail.com>
2024-02-13 17:31:07 +0900

    [Feat]#24 - WeatherDataInArray - 섭씨, 화씨 extension - WeatherManager의 getForecastWeather매소드에서 forecastData 배열에 time, weatherIcon, temperature, wind 데이터 넣기

--- Weather777/Network/WeatherManager.swift
+++ Weather777/Network/WeatherManager.swift
@@ -48,19 +48,58 @@ class WeatherManager {
         performRequest(with: url, completion: completion)
     }
     
-    // https://openweathermap.org/forecast5 5일치 3시간 단위 일기예보
-    //test API call : api.openweathermap.org/data/2.5/forecast?lat=37.4536&lon=126.7317&appid=3a33f61058f414d02d09e88bfa83117c
-    public func getForecastWeather(latitude: Double, longitude: Double, completion: @escaping(Result<WeatherData, NetworkError>) -> Void) {
+//    // https://openweathermap.org/forecast5 5일치 3시간 단위 일기예보
+//    //test API call : api.openweathermap.org/data/2.5/forecast?lat=37.4536&lon=126.7317&appid=3a33f61058f414d02d09e88bfa83117c
+//    public func getForecastWeather(latitude: Double, longitude: Double, completion: @escaping(Result<WeatherData, NetworkError>) -> Void) {
+//        LocationManager.shared.setLocation(latitude: latitude, longitude: longitude)
+//        guard let currentLocation = LocationManager.shared.currentLocation else {
+//            return completion(.failure(.badLocation))
+//        }
+//        
+//        guard let url = URL.urlForForecastForLocation(currentLocation, apiKey: apiKey) else {
+//            return completion(.failure(.badUrl))
+//        }
+//        performRequestForecast(with: url, completion: completion)
+//    }
+    public func getForecastWeather(latitude: Double, longitude: Double, completion: @escaping(Result<[(time: String, weatherIcon: String, temperature: Double, wind: String)], NetworkError>) -> Void) {
+        // 위치데이터
         LocationManager.shared.setLocation(latitude: latitude, longitude: longitude)
         guard let currentLocation = LocationManager.shared.currentLocation else {
             return completion(.failure(.badLocation))
         }
         
+        //API call
         guard let url = URL.urlForForecastForLocation(currentLocation, apiKey: apiKey) else {
             return completion(.failure(.badUrl))
         }
-        performRequestForecast(with: url, completion: completion)
+        
+        //API call에서 데이터를 forecastData 배열에 넣기
+        performRequestForecast(with: url) { result in
+            switch result {
+            case .success(let weatherData):
+                // 날씨 데이터를 성공적으로 받아왔을 때
+                var forecastData: [(time: String, weatherIcon: String, temperature: Double, wind: String)] = []
+                
+                for list in weatherData.list {
+                    let time = list.dtTxt
+                    let weatherIcon = list.weather.first?.icon ?? ""
+                    let temperature = Double(list.main.temp)
+                    let celsiusTemperature = temperature.toCelsius()
+                    let windSpeed = list.wind.speed
+                    
+                    let forecast = (time: time, weatherIcon: weatherIcon, temperature: celsiusTemperature, wind: "\(windSpeed) m/s")
+                    forecastData.append(forecast)
+                }
+                
+                completion(.success(forecastData))
+                
+            case .failure(let error):
+                // 날씨 데이터를 받아오는데 실패했을 때
+                completion(.failure(error))
+            }
+        }
     }
+    
     
 }
 
@@ -97,7 +136,6 @@ extension WeatherManager {
             //            print("Response: \(String(describing: response))")
             //            print("Error: \(String(describing: error))")
             
-            
             guard let data = data, error == nil else {
                 return completion(.failure(.noData))
             }
@@ -118,10 +156,9 @@ extension WeatherManager {
         
         URLSession.shared.dataTask(with: url) { data, Response, error in
             // 응답과 에러,데이터 출력 : 정상출력됨
-            print("ForecastData: \(String(describing: data))")
-            print("ForecastResponse: \(String(describing: Response))")
-            print("ForecastError: \(String(describing: error))")
-            
+//            print("ForecastData: \(String(describing: data))")
+//            print("ForecastResponse: \(String(describing: Response))")
+//            print("ForecastError: \(String(describing: error))")
             
             guard let data = data, error == nil else {
                 return completion(.failure(.noData))
